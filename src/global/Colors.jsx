b75f4749bbf801cd4a97a05d6a1e9cbc0d202b8c
kauanny-cmd <kauannyvieira8@gmail.com>
2023-02-18 15:36:34 -0300

    feat: coloração global

---
+++ src/global/Colors.jsx
@@ -0,0 +1,12 @@
+export default{
+    whiteColor: '#FFFFFF',
+    blackColor:'#000000',
+    blueColor:'#15C3D6',
+    emojiColor:'#FFD152',
+    blueCardColor:'#E6F7FB',
+    blueTextColor:'#B3DAE2',
+    backgroundMapColor:'#E5E5E5',
+    grayTextColor:'#5C8599',
+    backgroundRegisterColor:'#F2F3F5',
+    blueRegisterColor:'#15C3D6'
+}
