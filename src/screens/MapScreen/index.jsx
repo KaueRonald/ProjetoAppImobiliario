a21ebd5c6e65a6d3553f668912e987b15525ca0b
ldavi8533 <rafashpb@gmail.com>
2023-02-23 10:54:10 -0300

    feat: MapView

--- src/screens/MapScreen/index.jsx
+++ src/screens/MapScreen/index.jsx
@@ -1,9 +1,22 @@
 import { Button, TouchableOpacity, View , Text} from "react-native"
+import MapView from "react-native-maps"
 
 export default MapScreen = ({ navigation }) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Map screen</Text>
+            <MapView
+              style={{
+                height: '100%',
+                width: '100%',
+                position: 'absolute',		
+              }}
+              initialRegion={{
+                latitude:-6.88634,
+                longitude:-38.5614,
+                latitudeDelta: 0.008,
+                longitudeDelta: 0.008,
+              }}
+            />
             <TouchableOpacity>
                 <Button
                     title="Próximo"
