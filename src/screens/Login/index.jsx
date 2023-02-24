c8948d72f025d3e2bf6701c512725dabdb207adc
lucasttavares <zlucas.np@gmail.com>
2023-02-24 16:58:21 -0300

    test: uso do botao na tela de login

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,4 +1,6 @@
-import { Button, TouchableOpacity, View, Text } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
+import Button from "../../components/Button";
+import colors from "../../global/Colors";
 
 export default LoginScreen = ({ navigation }) => {
     return (
@@ -7,7 +9,11 @@ export default LoginScreen = ({ navigation }) => {
             <TouchableOpacity >
                 <Button
                     onPress={() => navigation.navigate('Home')}
-                    title="Login"
+                    color={colors.blackColor}
+                    border={6}
+                    width={343}
+                    height={52}
+                    title="LOG IN"
                 />
             </TouchableOpacity>
             <TouchableOpacity onPress={() => navigation.navigate('Register')}>
