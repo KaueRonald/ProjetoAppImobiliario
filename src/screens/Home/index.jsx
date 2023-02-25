db3525dc8c99af8875a6a0f84154ca3a30f8a589
ldavi8533 <rafashpb@gmail.com>
2023-02-24 21:25:05 -0300

    feat: iniciando os mapas

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,4 +1,5 @@
 import { Button, TouchableOpacity, View, Text } from "react-native";
+import MapView from "react-native-maps"
 
 import Colors from "../../global/Colors";
 
@@ -6,8 +7,21 @@ export default HomeScreen = ({ navigation }) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <View style={{width: 250, height: 100, backgroundColor: Colors.backgroundMapColor, alignItems:'center', justifyContent:'center'}}>
+            <MapView
+              style={{
+                height: '100%',
+                width: '100%',
+                position: 'absolute',		
+              }}
+              initialRegion={{
+                latitude:-6.88634,
+                longitude:-38.5614,
+                latitudeDelta: 0.008,
+                longitudeDelta: 0.008,
+              }}
+            >
+            </MapView>
                 <TouchableOpacity onPress={() => navigation.navigate('ViewMap')}>
-                    <Text>View Map</Text>
                 </TouchableOpacity>
             </View>
             <Text>DashBoard</Text>
--- src/screens/MapScreen/index.jsx
+++ src/screens/MapScreen/index.jsx
@@ -1,9 +1,36 @@
+import React, {useState } from "react";
 import { Button, TouchableOpacity, View , Text} from "react-native"
+import MapView,{ Marker }from 'react-native-maps'
 
 export default MapScreen = ({ navigation }) => {
+    const [marker, setMarker] = useState([]);
+
+    const handleNewMarker = (coordinate) => {
+      setMarker([...marker, coordinate]);
+    };
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Map screen</Text>
+            <MapView
+              onPress={(e) => handleNewMarker(e.nativeEvent.coordinate)}
+              style={{
+                height: '100%',
+                width: '100%',
+                position: 'absolute',		
+              }}
+              initialRegion={{
+                latitude:-6.88634,
+                longitude:-38.5614,
+                latitudeDelta: 0.008,
+                longitudeDelta: 0.008,
+              }}
+            >
+            {marker.length > 0 &&
+                marker.map((m) => {
+            return (
+              <Marker coordinate={m} key={Math.random().toString()}/>
+            );
+          })}
+            </MapView>
             <TouchableOpacity>
                 <Button
                     title="Próximo"
