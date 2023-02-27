04b661a14d3f2bc00da0cf023297c504398c7cae
Luis Felipe M <luis.felippe@academico.ifpb.edu.br>
2023-02-27 16:56:27 -0300

    Estruturando pagina de cadastro

--- src/screens/Register/index.jsx
+++ src/screens/Register/index.jsx
@@ -1,15 +1,49 @@
-import { Button, TouchableOpacity, View, Text } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
+import Input from "../../components/Input/"
+import Button from "../../components/Button/"
+import colors from "../../global/Colors";
 
 export default RegisterScreen = ({ navigation }) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Register Screen</Text>
+            <Text>Cadastre-se</Text>
+
+
             <TouchableOpacity>
+                <Input 
+                    iconName={'account'} 
+                    placeholder="E-mail" 
+                    autoCapitalize="none" 
+                    autoCorrect={false}
+                    keyboardType="email-address"
+                />
+                <Input 
+                    iconName={'lock-outline'} 
+                    placeholder="Senha" 
+                    autoCorrect={false}
+                    secureTextEntry 
+                    autoCapitalize="none" 
+                    keyboardType="default"
+                />
+                <Input 
+                    iconName={'lock-outline'} 
+                    placeholder="Confirme sua Senha" 
+                    autoCorrect={false}
+                    secureTextEntry 
+                    autoCapitalize="none" 
+                    keyboardType="default"
+                />
                 <Button
                     onPress={() => navigation.navigate('Home')}
-                    title="Register"
+                    color={colors.blackColor}
+                    border={6}
+                    width={343}
+                    height={52}
+                    title="REGISTER"
                 />
             </TouchableOpacity>
+
+
         </View>
     );
 }
