19fb79a2a5d54c587ec14ca082abaa31391c4ac5
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-02-28 08:59:25 -0300

    refactor: ajustando identação

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -3,12 +3,12 @@ import { useState } from 'react'
 import { View, StyleSheet, TextInput, TouchableOpacity } from "react-native"
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
-const Input = (props)=>{
+const Input = (props) => {
     const [sec, setSec] = useState(props.secureTextEntry)
 
-    return(
+    return (
         <View style={styles.container}>
-            <TextInput 
+            <TextInput
                 style={styles.input}
                 underlineColorAndroid="transparent"
                 placeholderTextColor={"#7B8794"}
@@ -16,33 +16,33 @@ const Input = (props)=>{
                 secureTextEntry={sec}
             />
             <Icon name={props.iconName} size={26} color="black" style={styles.icon} />
-            {props.secureTextEntry &&(
-                 <TouchableOpacity onPress={() => setSec(!sec)}>
-                <Icon 
-                    name={sec ? 'eye' : 'eye-off'} 
-                    size={26} 
-                    color="black" 
-                    style={styles.iconSecret}>
-                </Icon>
-            </TouchableOpacity>
+            {props.secureTextEntry && (
+                <TouchableOpacity onPress={() => setSec(!sec)}>
+                    <Icon
+                        name={sec ? 'eye' : 'eye-off'}
+                        size={26}
+                        color="black"
+                        style={styles.iconSecret}>
+                    </Icon>
+                </TouchableOpacity>
             )}
-           
+
         </View>
 
-        
+
     )
 }
 
 
 const styles = StyleSheet.create({
-    container:{
-        flexDirection:'row',
+    container: {
+        flexDirection: 'row',
         marginTop: 20
     },
-    input:{
-        height:50,
-        flex:1,
-        backgroundColor:'#F8F9FA',
+    input: {
+        height: 50,
+        flex: 1,
+        backgroundColor: '#F8F9FA',
         paddingLeft: 40,
         borderRadius: 8,
         fontSize: 18,
@@ -50,15 +50,15 @@ const styles = StyleSheet.create({
         borderWidth: 1,
 
     },
-    icon:{
-        position:'absolute',
-        left:10,
-        top:12,
+    icon: {
+        position: 'absolute',
+        left: 10,
+        top: 12,
     },
-    iconSecret:{
-        position:'absolute',
-        right:30,
-        top:12,
+    iconSecret: {
+        position: 'absolute',
+        right: 30,
+        top: 12,
     }
 })
 
