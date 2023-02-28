41d19f2a9d885e5dee9bf101941474c9026712ca
ldavi8533 <rafashpb@gmail.com>
2023-02-27 22:16:14 -0300

    feat: Rota de abrir mapa

--- src/screens/ViewMap/index.jsx
+++ src/screens/ViewMap/index.jsx
@@ -1,14 +1,23 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import MapView from "react-native-maps"
+import { View, Text,Image, TouchableOpacity } from "react-native";
+import MapView,{Marker} from "react-native-maps"
+import openMap from 'react-native-open-maps'
 
+import mapMarker from "../../../assets/map-marker.png";
 import Colors from "../../global/Colors";
 
 export default ViewMap = ({ navigation }) => {
+
+  function OpenMaps(){
+    openMap({latitude: -6.88634, longitude: -38.5614})
+  }
+
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>View Map</Text>
             <View style={{ width: 250, height: 100, backgroundColor: Colors.backgroundMapColor, alignItems: 'center', justifyContent: 'center' }}>
             <MapView
+              onPress={OpenMaps}
               style={{
                 height: '100%',
                 width: '100%',
@@ -21,6 +30,12 @@ export default ViewMap = ({ navigation }) => {
                 longitudeDelta: 0.008,
               }}
             >
+               <Marker
+                  icon={mapMarker}
+                  coordinate={{latitude: -6.88634,longitude: -38.5614}}
+                  key={Math.random().toString()}> 
+                  <Image source={require('../../../assets/Icon-map.png')} />
+                  </Marker>
             </MapView>
                 <TouchableOpacity onPress={() => navigation.navigate('ViewMap')}>
                     <Text></Text>
